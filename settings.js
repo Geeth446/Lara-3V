ec8d3fce510cc6f7e0098e1072cf123d33ea673b
Geeth446 <whybot34@gmail.com>
2025-11-05 21:47:52 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SJRWgSDQ#y6xRhDhCjy1pbMebQoGezqzRG4p-_7O9WtB2_zF5xO8", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "nZhH3bBa#_eMrZ3ES-3t_6LNqpl-SsZxGz3_GvnQm_KY7vUKiAQk", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "group",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
